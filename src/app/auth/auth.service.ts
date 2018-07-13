ec32427e786c6558c4bc7b26454fce11704a0d9c
Max Alva <alva85@gmail.com>
2018-07-13 16:11:42 -0500

    Dispatch - Activar desactivar loading

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,6 +6,9 @@ import Swal from 'sweetalert2';
 import * as firebase from 'firebase';
 import { map } from 'rxjs/operators';
 import { User } from '../models/user.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.actions';
 
 @Injectable({
     providedIn: 'root'
@@ -14,7 +17,8 @@ export class AuthService {
 
     constructor(private afAuth: AngularFireAuth,
         private router: Router,
-        private afDB: AngularFirestore) { }
+        private afDB: AngularFirestore,
+        private store: Store<AppState>) { }
 
     initAuthListener() {
         this.afAuth.authState.subscribe((fbUser: firebase.User) => {
@@ -23,6 +27,8 @@ export class AuthService {
     }
 
     crearUsuario(nombre: string, email: string, password: string) {
+
+        this.store.dispatch(new ActivarLoadingAction());
 
         this.afAuth.auth.createUserWithEmailAndPassword(email, password)
             .then(response => {
@@ -36,21 +42,27 @@ export class AuthService {
                 this.afDB.doc(`${user.uid}/usuario`).set(user)
                     .then(() => {
                         this.router.navigate(['/']);
+                        this.store.dispatch(new DesactivarLoadingAction());
                     });
 
             }).catch(error => {
                 console.log(error);
+                this.store.dispatch(new DesactivarLoadingAction());
                 Swal('Error al registrar', error.message, 'error');
             });
     }
 
     login(email: string, password: string) {
+        this.store.dispatch(new ActivarLoadingAction());
+
         this.afAuth.auth.signInWithEmailAndPassword(email, password)
             .then(response => {
                 this.router.navigate(['/']);
+                this.store.dispatch(new DesactivarLoadingAction());
             }).catch(error => {
                 console.log(error);
                 Swal('Error en el login', error.message, 'error');
+                this.store.dispatch(new DesactivarLoadingAction());
             });
     }
 
