8e6e00d1aee5fe459a748140559d9cc5c839edb1
Max Alva <alva85@gmail.com>
2018-07-13 17:47:44 -0500

    model de ingreso y egreso

---
+++ src/app/models/ingreso-egreso.model.ts
@@ -0,0 +1,14 @@
+
+export class IngresoEgreso {
+    descripcion: string;
+    monto: number;
+    tipo: string;
+    uid?: string;
+
+    constructor(dataObj) {
+        this.descripcion = dataObj && dataObj.descripcion || null;
+        this.monto = dataObj && dataObj.monto || null;
+        this.tipo = dataObj && dataObj.tipo || null;
+        this.uid = dataObj && dataObj.uid || null;
+    }
+}
