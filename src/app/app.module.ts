cdc7e5cceba5ee62e23a7858bbdd4496d102b21b
Max Alva <alva85@gmail.com>
2018-07-13 18:03:56 -0500

    Pantalla para agregar ingresos y egresos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-rounting.module';
 
@@ -42,6 +42,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
         BrowserModule,
         AppRoutingModule,
         FormsModule,
+        ReactiveFormsModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFirestoreModule,
         AngularFireAuthModule,
--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-ingreso-egreso',
-  templateUrl: './ingreso-egreso.component.html',
-  styles: []
+    selector: 'app-ingreso-egreso',
+    templateUrl: './ingreso-egreso.component.html',
+    styles: []
 })
 export class IngresoEgresoComponent implements OnInit {
 
-  constructor() { }
+    formData: FormGroup;
+    tipo = 'ingreso';
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.formData = new FormGroup({
+            descripcion: new FormControl('', Validators.required),
+            monto: new FormControl(0, Validators.min(1))
+        });
+    }
+
+    crearIngresoEgreso() {
+        console.log(this.formData.value);
+    }
 
 }
