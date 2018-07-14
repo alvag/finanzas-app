160257487d570b17ae99f341071e8d350060be36
Max Alva <alva85@gmail.com>
2018-07-14 12:50:15 -0500

    Mostrar todos los elementos de ingreso y egreso

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { IngresoEgreso } from '../../models/ingreso-egreso.model';
+import { LoggerService } from '../../services/logger/logger.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-detalle',
-  templateUrl: './detalle.component.html',
-  styles: []
+    selector: 'app-detalle',
+    templateUrl: './detalle.component.html',
+    styles: []
 })
-export class DetalleComponent implements OnInit {
+export class DetalleComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    items: IngresoEgreso[];
+    subscription: Subscription = new Subscription();
 
-  ngOnInit() {
-  }
+    constructor(private store: Store<AppState>,
+        private logger: LoggerService) { }
+
+    ngOnInit() {
+        this.subscription = this.store.select('ingresoEgreso')
+            .subscribe(data => {
+                this.logger.info(data.items);
+                this.items = data.items;
+            });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+
+    borrarItem(uid: string) {
+
+    }
 
 }
