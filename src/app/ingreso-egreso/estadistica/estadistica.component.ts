85850ad501ef54f8d690d3db4c3b7af653483bf9
Max Alva <alva85@gmail.com>
2018-07-14 13:40:51 -0500

    Dashboard - estadística básica

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { IngresoEgreso } from '../../models/ingreso-egreso.model';
 
 @Component({
-  selector: 'app-estadistica',
-  templateUrl: './estadistica.component.html',
-  styles: []
+    selector: 'app-estadistica',
+    templateUrl: './estadistica.component.html',
+    styles: []
 })
 export class EstadisticaComponent implements OnInit {
 
-  constructor() { }
+    ingresos: number;
+    egresos: number;
 
-  ngOnInit() {
-  }
+    cuantosIngresos: number;
+    cuantosEgresos: number;
+
+    subscription: Subscription = new Subscription();
+
+    constructor(private store: Store<AppState>) { }
+
+    ngOnInit() {
+        this.subscription = this.store.select('ingresoEgreso')
+            .subscribe(ingresoEgreso => this.contarIngresosEgresos(ingresoEgreso.items))
+    }
+
+    contarIngresosEgresos(items: IngresoEgreso[]) {
+        this.ingresos = 0;
+        this.egresos = 0;
+        this.cuantosEgresos = 0;
+        this.cuantosIngresos = 0;
+
+        items.forEach(item => {
+            if (item.tipo === 'ingreso') {
+                this.cuantosIngresos++;
+                this.ingresos += item.monto;
+            } else {
+                this.cuantosEgresos++;
+                this.egresos += item.monto;
+            }
+        });
+    }
 
 }
