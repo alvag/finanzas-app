9c4f801a221ac34022f93b3a6c62ac5286c0b254
Max Alva <alva85@gmail.com>
2018-07-14 11:01:18 -0500

    Optimizaciones del loading

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IngresoEgreso } from '../models/ingreso-egreso.model';
 import { IngresoEgresoService } from './ingreso-egreso.service';
 import { LoggerService } from '../services/logger/logger.service';
 import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { Subscription } from 'rxjs';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.actions';
 
 
 @Component({
@@ -11,22 +15,33 @@ import Swal from 'sweetalert2';
     templateUrl: './ingreso-egreso.component.html',
     styles: []
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
 
     formData: FormGroup;
     tipo = 'ingreso';
+    loadingSubscription: Subscription = new Subscription();
+    cargando: boolean;
 
     constructor(private ingresoEgresoService: IngresoEgresoService,
-        private logger: LoggerService) { }
+        private logger: LoggerService,
+        private store: Store<AppState>) { }
 
     ngOnInit() {
+
+        this.loadingSubscription = this.store.select('ui').subscribe(ui => this.cargando = ui.isLoading);
+
         this.formData = new FormGroup({
             descripcion: new FormControl('', Validators.required),
             monto: new FormControl(0, Validators.min(1))
         });
     }
 
+    ngOnDestroy() {
+        this.loadingSubscription.unsubscribe();
+    }
+
     crearIngresoEgreso() {
+        this.store.dispatch(new ActivarLoadingAction());
         const ingresoEgreso = new IngresoEgreso({
             ...this.formData.value,
             tipo: this.tipo
@@ -35,11 +50,11 @@ export class IngresoEgresoComponent implements OnInit {
         this.ingresoEgresoService.crearIngresoEgreso(ingresoEgreso)
             .then(response => {
                 this.logger.info(response);
-
                 Swal('Creado', ingresoEgreso.descripcion, 'success');
-
+                this.store.dispatch(new DesactivarLoadingAction());
                 this.formData.reset({ monto: 0 });
             }).catch(error => {
+                this.store.dispatch(new DesactivarLoadingAction());
                 this.logger.info(error);
             });
     }
